503e1bdfc7b70dcddaeb12f1134bd5db7b1a329d
Roshan123 <roshan555routh@gmail.com>
2022-10-11 15:07:06 +0530

    Logical Operators & Ternary Operator

---
+++ Day 3 programs/problem1.js
@@ -0,0 +1,14 @@
+ // Given the year of birth, if the age is between 13 and 19 (both included) print Teenage and in between 20 and 29 (both included) print Twenties
+
+
+  let year_Barth=1999;
+
+  let age=2022-year_Barth;
+
+   if(age>=13 && age<=19)
+   {
+     console.log("Teenage")
+   }else if(age>=20 && age<=29)
+   {
+     console.log("Twenties")
+   }
---
+++ Day 3 programs/problem4.js
@@ -0,0 +1,18 @@
+
+  // Given 3 numbers (all different values), print which is greatest
+
+     let a=10;
+     let b=15;
+     let c=16;
+
+  if(a>=b && a>=c)
+  {
+    console.log("a is greatest")
+  }
+  else if(b>=a && b>=c)
+  {
+    console.log("b is greatest")
+  }
+  else{
+     console.log("c is greatest")
+  }
