ff1df051efee15b09f70182d3fa2f7c2e333294f
Roshan123 <roshan555routh@gmail.com>
2022-10-12 20:37:21 +0530

    while loop method

---
+++ Day 4 programs/problem2.js
@@ -0,0 +1,11 @@
+// Problem 2: Print the odd numbers from 0 till the given limit
+
+  let i=0;
+  let limit=50;
+
+ while (i<=50){
+   if(i%2 != 0){
+     console.log(i)
+   }
+   i++
+ }
---
+++ Day 4 programs/problem4.js
@@ -0,0 +1,19 @@
+// Problem 4: Print the average of even numbers from 1 to 100 (both included)
+
+  let i=1;
+
+  let count=0;
+  let sum=0
+
+let limit=100;
+
+ while(i<=100){
+   count++
+   sum+=i
+   i++
+ }
+
+ let average=sum/count;
+
+ console.log(average)
+
